0676a6abbd7bb981afcbf05cb39d97d5abfdb172
House-lovers7 <taisuke.got@gmail.com>
2021-09-12 17:24:12 +0900

    impotの使い方

--- def1.py
+++ def1.py
@@ -3,20 +3,3 @@ def sayhello():
 
 
 sayhello()
-
-
-def postTaxPrice(price):
-    ans = price * 1.08
-    return ans
-
-
-print(postTaxPrice(100), "円")
-print(postTaxPrice(500), "円")
-print(postTaxPrice(990), "円")
-
-
-def myfunc():
-    print("Hello")
-
-
-myfunc()
---
+++ import1.py
@@ -0,0 +1,6 @@
+import tax
+
+
+print(tax.postTaxPrice(100), "円")
+print(tax.postTaxPrice(500), "円")
+print(tax.postTaxPrice(990), "円")
