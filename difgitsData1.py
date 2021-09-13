b5701a38d178e2dd5c7952c64512880138705f84
House-lovers7 <taisuke.got@gmail.com>
2021-09-13 09:57:47 +0900

    数字データ情報の画像取得

--- difgitsData1.py
+++ difgitsData1.py
@@ -1,7 +1,11 @@
 import sklearn.datasets
+import matplotlib.pyplot as pit
 
 digits = sklearn.datasets.load_digits()
 
 print("データの個数＝", len(digits.images))
-print("画像データ＝", digits.images[0])
-print("何の数字か＝", digits.target[0])
+print("画像データ＝", digits.images[8])
+print("何の数字か＝", digits.target[8])
+
+pit.imshow(digits.images[9], cmap="Greys")
+pit.show()
